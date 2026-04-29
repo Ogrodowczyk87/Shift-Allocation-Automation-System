4e1ecd1c62dae830297dbd7cc36588002edc34d8
Rafal Ogrodowczyk <r.ogrodowczyk87@gmail.com>
2026-04-29 14:24:13 +0100

    Stabilize Cognito OIDC state handling

--- src/main.tsx
+++ src/main.tsx
@@ -5,13 +5,16 @@ import { WebStorageStateStore } from 'oidc-client-ts'
 import './index.css'
 import App from './App.tsx'
 
+const storage = new WebStorageStateStore({ store: window.localStorage })
+
 const cognitoAuthConfig = {
   authority: import.meta.env.VITE_COGNITO_AUTHORITY,
   client_id: import.meta.env.VITE_COGNITO_CLIENT_ID,
   redirect_uri: import.meta.env.VITE_COGNITO_REDIRECT_URI,
   response_type: 'code',
   scope: 'openid email',
-  userStore: new WebStorageStateStore({ store: window.localStorage }),
+  userStore: storage,
+  stateStore: storage,
   onSigninCallback: () => {
     window.history.replaceState({}, document.title, window.location.pathname)
   },
